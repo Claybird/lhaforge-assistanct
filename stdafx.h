3d1c6bf7ea2c31ee853313095e926997b98443b9
Claybird <claybird.without.wing@gmail.com>
2024-05-19 20:53:02 +0900

    added std::filesystem

--- stdafx.h
+++ stdafx.h
@@ -6,22 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-#include <tchar.h>
-#include <assert.h>
-#include <list>
 #include <vector>
-#include <time.h>
 #include <algorithm>
 #include <string>
+#include <filesystem>
 
 #include <shlobj.h>
 
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
-#define COUNTOF(x) (sizeof(x)/sizeof(x[0]))
-#define ASSERT(x)	assert(x)
-
-//TODO
-#define TRACE
-//TODO:remove
-#include <atlpath.h>
