88619d7c2a5d5365dbe6148af773fce33b64cfe1
Claybird <claybird.without.wing@gmail.com>
2024-05-19 20:53:53 +0900

    updated Utility
    removed unused functions
    modernized functions

--- Utility.cpp
+++ Utility.cpp
@@ -2,140 +2,46 @@
 #include "resource.h"
 #include "Utility.h"
 
-#if defined(DEBUG) || defined(_DEBUG)
-void UtilDebugTrace(LPCTSTR pszFormat, ...)
-{
-	va_list	args;
 
-	va_start(args, pszFormat);
-	CString str;
-	str.FormatV(pszFormat,args);
-	va_end(args);
-
-	OutputDebugString(str);
-}
-
-void TraceLastError()
-{
-	LPVOID lpMsgBuf;
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL, GetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // デフォルト言語
-		(LPTSTR)&lpMsgBuf, 0, NULL);
-	TRACE(_T("API Error : %s"),lpMsgBuf);
-	LocalFree(lpMsgBuf);
-}
-
-#endif
-
-//エラーメッセージ表示
-int ErrorMessage(LPCTSTR msg)
-{
-	TRACE(_T("ErrorMessage:")),TRACE(msg),TRACE(_T("\n"));
-	return MessageBox(NULL,msg,UtilGetMessageCaption(),MB_OK|MB_ICONSTOP);
-}
-
-//メッセージキャプションを取得
-LPCTSTR UtilGetMessageCaption()
-{
-	const static CString strCaption(MAKEINTRESOURCE(IDS_MESSAGE_CAPTION));
-	return strCaption;
-}
-
-void UtilGetLastErrorMessage(CString &strMsg)
-{
-	//実行エラー
-	LPVOID lpMsgBuf;
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,GetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPTSTR)&lpMsgBuf, 0, NULL);
-
-	strMsg=(LPCTSTR)lpMsgBuf;
-	LocalFree(lpMsgBuf);
-}
-
-
-
-//コマンドライン引数を取得(個数を返す)
-int UtilGetCommandLineParams(std::vector<CString> &rParams)
+std::vector<std::wstring> UtilGetCommandLineArgs()
 {
 	int nArgc=0;
-	LPWSTR *lplpArgs=CommandLineToArgvW(GetCommandLine(), &nArgc);
-	rParams.resize(nArgc);
-	for(int i=0;i<nArgc;i++){
-		rParams[i]=lplpArgs[i];
+	std::vector<std::wstring> args;
+	LPWSTR *lplpArgs=CommandLineToArgvW(GetCommandLineW(), &nArgc);
+	args.resize(nArgc);
+	for (int i = 0; i < nArgc; i++) {
+		args[i] = lplpArgs[i];
 	}
 	LocalFree(lplpArgs);
-	return nArgc;
+	return args;
 }
 
-
-//INIに数字を文字列として書き込む
-BOOL UtilWritePrivateProfileInt(LPCTSTR lpAppName,LPCTSTR lpKeyName,LONG nData,LPCTSTR lpFileName)
+bool UtilCheckINISectionExists(const std::wstring& appName, const std::wstring &fileName)
 {
-	TCHAR Buffer[32]={0};
-	wsprintf(Buffer,_T("%ld"),nData);
-	return ::WritePrivateProfileString(lpAppName,lpKeyName,Buffer,lpFileName);
-}
-
-
-//INIに指定されたセクションがあるならtrueを返す
-bool UtilCheckINISectionExists(LPCTSTR lpAppName,LPCTSTR lpFileName)
-{
-	TCHAR szBuffer[10];
-	DWORD dwRead=GetPrivateProfileSection(lpAppName,szBuffer,9,lpFileName);
+	const size_t bufsize = 16;
+	wchar_t szBuffer[bufsize] = {};
+	DWORD dwRead = GetPrivateProfileSectionW(appName.c_str(), szBuffer, bufsize - 1, fileName.c_str());
 	return dwRead>0;
 }
 
-//フルパスかつ絶対パスの取得
-PATHERROR UtilGetCompletePathName(CString &_FullPath,LPCTSTR lpszFileName)
+//executable name
+std::filesystem::path UtilGetModulePath()
 {
-	ASSERT(lpszFileName&&_tcslen(lpszFileName)>0);
-	if(!lpszFileName||_tcslen(lpszFileName)<=0){
-		TRACE(_T("ファイル名が指定されていない\n"));
-		return PATHERROR_INVALID;
-	}
-
-	//---絶対パス取得
-	TCHAR szAbsPath[_MAX_PATH+1]={0};
-	{
-		TCHAR Buffer[_MAX_PATH+1]={0};	//ルートかどうかのチェックを行う
-		_tcsncpy_s(Buffer,lpszFileName,_MAX_PATH);
-		PathAddBackslash(Buffer);
-		if(PathIsRoot(Buffer)){
-			//ドライブ名だけが指定されている場合、
-			//_tfullpathはそのドライブのカレントディレクトリを取得してしまう
-			_tcsncpy_s(szAbsPath,lpszFileName,_MAX_PATH);
-		}
-		else if(!_tfullpath(szAbsPath,lpszFileName,_MAX_PATH)){
-			TRACE(_T("絶対パス取得失敗\n"));
-			return PATHERROR_ABSPATH;
+	std::wstring name;
+	name.resize(256);
+	for (;;) {
+		DWORD bufsize = (DWORD)name.size();
+		auto nCopied = GetModuleFileNameW(GetModuleHandleW(nullptr), &name[0], bufsize);
+		if (nCopied < bufsize) {
+			break;
+		} else {
+			name.resize(name.size() * 2);
 		}
 	}
-
-	if(!PathFileExists(szAbsPath)&&!PathIsDirectory(szAbsPath)){
-		//パスがファイルもしくはディレクトリとして存在しないなら、エラーとする
-		TRACE(_T("ファイルが存在しない\n"));
-		return PATHERROR_NOTFOUND;
-	}
-	if(!GetLongPathName(szAbsPath,szAbsPath,_MAX_PATH)){
-		TRACE(_T("ロングファイル名取得失敗\n"));
-		return PATHERROR_LONGNAME;
-	}
-	_FullPath=szAbsPath;
-	return PATHERROR_NONE;
+	return name.c_str();
 }
 
-//自分のプログラムのおいてあるディレクトリのパス名を返す
-LPCTSTR UtilGetModuleDirectoryPath()
+std::filesystem::path UtilGetModuleDirectoryPath()
 {
-	static TCHAR s_szDirPath[_MAX_PATH+1]={0};
-	if(s_szDirPath[0]!=_T('\0'))return s_szDirPath;
-
-	GetModuleFileName(GetModuleHandle(NULL), s_szDirPath, _MAX_PATH);	//本体のパス取得
-	PathRemoveFileSpec(s_szDirPath);
-	return s_szDirPath;
+	return std::filesystem::path(UtilGetModulePath()).parent_path();
 }
--- Utility.h
+++ Utility.h
@@ -1,72 +1,14 @@
-//LhaForge用
-
-//デバッグ用関数および便利な関数群
 #pragma once
-//#pragma warning(disable:4786)
 
-#if defined(_DEBUG) || defined(DEBUG)
+std::vector<std::wstring> UtilGetCommandLineArgs();
 
-void TraceLastError();
+bool UtilCheckINISectionExists(const std::wstring& appName, const std::wstring& fileName);
 
-#else
-// Releaseのとき
-#define TraceLastError
-
-#endif	//_DEBUG
-
-//=============================================
-// 共通便利関数
-//=============================================
-
-//エラーメッセージを表示
-int ErrorMessage(LPCTSTR);
-//メッセージキャプションを取得
-LPCTSTR UtilGetMessageCaption();
-void UtilGetLastErrorMessage(CString &strMsg);
-
-#define BOOL2bool(x)	(FALSE!=x)
-
-//コマンドライン引数を取得(個数を返す)
-int UtilGetCommandLineParams(std::vector<CString> &rParams);
-
-//INIに数字を文字列として書き込む
-BOOL UtilWritePrivateProfileInt(LPCTSTR lpAppName,LPCTSTR lpKeyName,LONG nData,LPCTSTR lpFileName);
-
-//INIに指定されたセクションがあるならtrueを返す
-bool UtilCheckINISectionExists(LPCTSTR lpAppName,LPCTSTR lpFileName);
-
-//指定されたmapがキーを持っているかどうか
 template <typename mapclass,typename keyclass>
 bool has_key(const mapclass &theMap,keyclass theKey){
 	return theMap.find(theKey)!=theMap.end();
 }
 
+std::filesystem::path UtilGetModulePath();
+std::filesystem::path UtilGetModuleDirectoryPath();
 
-enum PATHERROR{
-	PATHERROR_NONE,		//成功
-	PATHERROR_INVALID,	//パラメータ指定が不正
-	PATHERROR_ABSPATH,	//絶対パスの取得に失敗
-	PATHERROR_NOTFOUND,	//ファイルもしくはフォルダが見つからない
-	PATHERROR_LONGNAME,	//ロングファイル名取得失敗
-};
-//フルパスかつ絶対パスの取得
-PATHERROR UtilGetCompletePathName(CString &_FullPath,LPCTSTR lpszFileName);
-
-//自分のプログラムのおいてあるディレクトリのパス名を返す
-LPCTSTR UtilGetModuleDirectoryPath();
-
-//指定された値が配列中にあればそのインデックスを探す;無ければ-1
-template <typename arrayclass,typename valueclass>
-int index_of(const arrayclass &theArray,valueclass theValue){
-	for(unsigned int i=0;i<theArray.size();++i){
-		if(theArray[i]==theValue){
-			return (signed)i;
-		}
-	}
-	return -1;
-}
-//コンテナの要素を削除する
-template <typename arrayclass,typename valueclass>
-void remove_item(arrayclass &theArray,const valueclass &theValue){
-	theArray.erase(std::remove(theArray.begin(), theArray.end(), theValue), theArray.end());
-}
